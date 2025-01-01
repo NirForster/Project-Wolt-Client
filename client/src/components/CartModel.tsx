0f528c39288850d019455ef2629f3e38ea150df4
HavivBitton <havivbiton4@gmail.com>
2025-01-01 14:34:30 +0200

    work on cart components

---
+++ client/src/components/CartModel.tsx
@@ -0,0 +1,29 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const CartModel = () => {
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent side={"left"} className="w-[250px] ">
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default CartModel;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import CartModel from "@/components/CartModel";
+import CartTabs from "@/components/CartTabs";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      {/* <CartTabs /> */}
+      <CartModel />
+    </div>
+  );
+};
+
+export default HomePage;
