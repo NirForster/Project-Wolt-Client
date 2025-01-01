17bc6f77e297809d5d206e44f6ad516f3a0a460d
NirForster <162992599+NirForster@users.noreply.github.com>
2025-01-01 14:41:58 +0200

    inserted some placeholder pages and components plus routing

---
+++ client/src/pages/RestaurantPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RestaurantPage = () => {
+  return <div>RestaurantsPage</div>;
+};
+
+export default RestaurantPage;
---
+++ client/src/pages/discovery-pages/DiscoveryStorePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DiscoveryStorePage = () => {
+  return <div>DiscoveryStorePage</div>;
+};
+
+export default DiscoveryStorePage;
