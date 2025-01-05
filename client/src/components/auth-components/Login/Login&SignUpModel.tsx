85cdf80c9477fd4e53af4afc3b43ba08ba2d2b07
NirForster <162992599+NirForster@users.noreply.github.com>
2025-01-05 22:53:08 +0200

    changed opacity

--- client/src/components/auth-components/Login/Login&SignUpModel.tsx
+++ client/src/components/auth-components/Login/Login&SignUpModel.tsx
@@ -7,7 +7,7 @@ interface LoginSignUpModelProps {
 
 const LoginSignUpModel: React.FC<LoginSignUpModelProps> = ({ onClose }) => {
   return (
-    <div className="  fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="  fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
       <div className="bg-white rounded-lg shadow-lg h-[670px] max-w-[500px] relative">
         {/* Modal Content */}
         <LoginForm onClose={onClose} />
