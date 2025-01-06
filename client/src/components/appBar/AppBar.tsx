d05e6591e4aa13cdedad21434606fe55f58270db
HavivBitton <havivbiton4@gmail.com>
2025-01-06 13:47:01 +0200

    save change on app bar

--- client/src/components/appBar/AppBar.tsx
+++ client/src/components/appBar/AppBar.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import woltLogo from "../../assets/dummyData/Wolt-Logo-b&w.png";
 import { SlHome } from "react-icons/sl";
+import { log } from "console";
 
 const AppBar = () => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
   return (
     <div className="flex  h-[70px] bg-white w-full px-5 py-3 border-b border-gray-200">
       <div className="flex-1">
@@ -15,7 +19,11 @@ const AppBar = () => {
               3
             </span>
           </div>
-          <button className="px-4 py-2 text-gray-600 bg-gray-100 rounded-full hover:bg-gray-200">
+          <button
+            className={`px-4 py-2 text-gray-600 bg-gray-100 rounded-full hover:bg-gray-200 ${
+              isSearchActive ? "opacity-0" : "opacity-100"
+            }`}
+          >
             HB
           </button>
         </div>
@@ -24,6 +32,8 @@ const AppBar = () => {
         <div className="flex items-center px-4 py-2 bg-gray-100 rounded-full">
           <input
             type="text"
+            onFocus={() => setIsSearchActive(true)}
+            onBlur={() => setIsSearchActive(false)}
             placeholder="חיפוש באתר"
             className="w-full text-sm bg-transparent outline-none"
           />
@@ -31,9 +41,20 @@ const AppBar = () => {
         </div>
       </div>
       <div className="flex-1 ">
-        <div className="flex h-full justify-end">
-          <SlHome />
-          <img src={woltLogo} alt="eolt-logo" />
+        <div className="flex items-center h-full justify-end ">
+          {/* Address */}
+          <button
+            className={`text-sm font-medium text-blue-600 hover:underline`}
+          >
+            בית (נחלת בנימין 42)
+          </button>
+          {/* Home Icon */}
+          <div className="p-2  text-blue-600 ml-1 h-8 bg-blue-100 rounded-full items-center">
+            <SlHome size={16} />
+          </div>
+          <div className="flex w-[80px] justify-end ml-2">
+            <img src={woltLogo} alt="wolt-logo" />
+          </div>
         </div>
       </div>
     </div>
