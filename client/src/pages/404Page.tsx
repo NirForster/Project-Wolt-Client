b32f364673d768163b234c187c4d2a726bb0de78
NirForster <162992599+NirForster@users.noreply.github.com>
2025-01-16 14:40:30 +0200

    edited error page

--- client/src/pages/404Page.tsx
+++ client/src/pages/404Page.tsx
@@ -30,7 +30,7 @@ const Error404Page: React.FC = () => {
   return (
     <main id="mainContent" className="flex flex-col ">
       <AppBar />
-      <div className="flex flex-col items-center justify-center min-h-screen bg-white text-center px-6 h-[100vh]">
+      <div className="flex flex-col items-center justify-start pt-12 lg:justify-center min-h-screen bg-white text-center px-6 h-[100vh]">
         {/* Lottie Animation */}
         <div className="relative mb-8 ">
           {animationData ? (
@@ -44,12 +44,12 @@ const Error404Page: React.FC = () => {
           )}
         </div>
         {/* Error Message */}
-        <h1 className="text-3xl font-bold text-woltColors-textDefault mb-4">
+        <h1 className="text-[2rem] line-[2.5rem] mb-6 md:text-[46px] font-bold text-woltColors-textDefault font-woltHeader ">
           Something unexpected happened
         </h1>
 
         {/* Description */}
-        <p className="text-lg text-woltColors-textSubdued mb-6">
+        <p className="text-sm md:text-lg text-woltColors-textSubdued mb-6">
           Unfortunately, we couldn't find the content you were looking for.
           Sorry for the hassle!
         </p>
@@ -57,7 +57,7 @@ const Error404Page: React.FC = () => {
         {/* Back to Wolt Button */}
         <button
           onClick={handleBackToHome}
-          className="px-6 py-3 bg-woltColors-brandBg text-white font-semibold rounded-lg hover:bg-woltColors-brandHovered transition duration-300"
+          className="px-6 py-3 bg-[#ebf7fd] text-woltColors-brandBg font-semibold rounded-lg hover:bg-[#d6effa] text-[1rem]"
         >
           Back to Home
         </button>
