9c5ce6f61dc005423f20cc02254c0e42366c48dc
NirForster <162992599+NirForster@users.noreply.github.com>
2025-01-22 12:10:22 +0200

    deleted AppBarHomePage

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,15 +1,13 @@
-// import AppBarHomePage from "@/components/main-layout-components/appBar/AppBarHomePage";
 import RotatingQuoteHero from "@/components/landing-page-section-components/RotatingQuoteHero";
 import ExploreCitiesSection from "@/components/landing-page-section-components/ExploreCitiesSection";
 import WhatIsWoltSection from "@/components/landing-page-section-components/WhatIsWoltSection";
 import DownloadWoltSectionMobile from "@/components/landing-page-section-components/DownloadWoltSectionMobile";
 import DownloadWoltSectionDesktop from "@/components/landing-page-section-components/DownloadWoltSectionDesktop";
-import AppBar from "@/components/main-layout-components/appBar/AppBar";
+import AppBar from "@/components/main-layout-components/AppBar";
 
 const LandingPage = () => {
   return (
     <div>
-      {/* <AppBarHomePage /> */}
       <AppBar />
       <RotatingQuoteHero />
       <ExploreCitiesSection />
