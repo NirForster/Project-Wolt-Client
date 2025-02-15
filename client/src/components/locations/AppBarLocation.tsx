37f8de2a9cb064d4c6bbb09039edffe306eb9bcc
NirForster <162992599+NirForster@users.noreply.github.com>
2025-02-15 19:22:33 +0200

    edited the app bar a little

--- client/src/components/locations/AppBarLocation.tsx
+++ client/src/components/locations/AppBarLocation.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { BsBuildingsFill } from "react-icons/bs";
-import { SlHome, SlLocationPin } from "react-icons/sl";
+import { SlHome } from "react-icons/sl";
 import { userContext } from "../../providers/userContext";
 
 const AppBarLocation = () => {
@@ -20,7 +20,19 @@ const AppBarLocation = () => {
     if (currentLoc?.type === "Work") {
       return <BsBuildingsFill size={16} />;
     }
-    return <SlLocationPin size={16} />; // Default icon
+    return (
+      <svg
+        viewBox="0 0 24 24"
+        className="w-5 h-5 fill-current text-woltColors-brandBg"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M6 9.5C6 12.813 8.687 15.5 12 15.5C15.312 15.497 17.997 12.813 18 9.5C18 6.187 15.313 3.5 12 3.5C8.687 3.5 6 6.187 6 9.5ZM2.5 9.5C2.506 4.256 6.756 0.006 12 0C17.244 0.006 21.493 4.255 21.5 9.499C21.5 16.044 14.958 21.987 12.958 23.653C12.402 24.114 11.597 24.114 11.041 23.653C9.037 21.987 2.5 16.044 2.5 9.5ZM14 9C14 10.1046 13.1046 11 12 11C10.8954 11 10 10.1046 10 9C10 7.89543 10.8954 7 12 7C13.1046 7 14 7.89543 14 9Z"
+        ></path>
+      </svg>
+    ); // Default icon
   };
 
   // useEffect(() => {
@@ -44,12 +56,14 @@ const AppBarLocation = () => {
     <div className="flex items-center">
       {/* Location Icon */}
       <div
-        className={`flex p-2 text-blue-600 mr-1 h-8 bg-blue-100 rounded-full items-center `}
+        className={`flex p-2 text-woltColors-brandBg mr-1 h-8 bg-[#ebf7fd] rounded-full items-center `}
       >
         {loggedInUserLocationIcon()}
       </div>
       {/* Address */}
-      <p className={`truncate-text text-sm font-medium text-blue-600  `}>
+      <p
+        className={`truncate-text text-sm font-medium text-woltColors-brandBg  `}
+      >
         {currentLoc
           ? `${currentLoc?.type} (${currentLoc?.address})`
           : "There is no location saved"}
